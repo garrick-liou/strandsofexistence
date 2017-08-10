69207726d5144c169c666288c006af2ce8771aea
Eli K <esklein@ucsc.edu>
2017-08-09 21:08:53 -0700

    pass 1, seems to still be functional after cleaning the code

---
+++ js/Tile.js
@@ -0,0 +1,16 @@
+function Tile(game, x, y, name, group){
+    Phaser.Sprite.call(this, game, x, y, 'atlas', name);
+    game.add.existing(this);
+    group.add(this);
+}
+Tile.prototype = Object.create(Phaser.Sprite.prototype);
+Tile.prototype.constructor = Tile;
+
+function Button(game, x, y, xCoord, yCoord, group, func){
+    Phaser.Button.call(this, game, x, y, 'atlas', function(){func(xCoord, yCoord)}, window, 'ButtonNorm', 'ButtonHover', 'ButtonHover');
+    game.add.existing(this);
+    group.add(this);
+    this.kill();
+}
+Button.prototype = Object.create(Phaser.Button.prototype);
+Button.prototype.constructor = Button;
