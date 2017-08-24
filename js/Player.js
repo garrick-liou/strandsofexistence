ed3f0037f2e3dcfef1582ece3839f1cd530c19e8
Eli K <esklein@ucsc.edu>
2017-08-23 21:35:28 -0700

    changed spawn locations because I screwed up a bit for a sec

--- js/Player.js
+++ js/Player.js
@@ -1,10 +1,10 @@
 var coords = [
       [0, 0],
-      [2, 0],
+      [3, 0],
       [0, 2],
-      [2, 2],
+      [3, 2],
       [0, 4],
-      [2, 4]
+      [3, 4]
 ]
 
 function LifeBarContainer(parent, x, y){
